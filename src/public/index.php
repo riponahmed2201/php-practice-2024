d07b525b86c5c4c5ed26c2cf04ede60e2d6493e6
ripon <riponahmed2201@gmail.com>
2024-02-12 12:49:43 +0600

    object learning

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../Transaction.php';
+// require_once '../Transaction.php';
 
 //Classes & Objects
 // $transaction = new Transaction(100, 'Transaction 1');
@@ -24,12 +24,31 @@ require_once '../Transaction.php';
 //     ->applyDiscount(10)
 //     ->getAmount();
 
-$transaction1 = (new Transaction(100, 'Transaction 1'))
-    ->addTax(8)
-    ->applyDiscount(10);
+// $transaction1 = (new Transaction(100, 'Transaction 1'))
+//     ->addTax(8)
+//     ->applyDiscount(10);
 
-$transaction2 = (new Transaction(200, 'Transaction 1'))
-    ->addTax(8)
-    ->applyDiscount(15);
+// $transaction2 = (new Transaction(200, 'Transaction 1'))
+//     ->addTax(8)
+//     ->applyDiscount(15);
 
-var_dump($transaction1->getAmount(), $transaction2->getAmount());
+// var_dump($transaction1->getAmount(), $transaction2->getAmount());
+
+
+//==================== Object =====================
+
+// $str = '{"a":1, "b":2,"c":3 }';
+
+// // $arr = json_decode($str, true); // convert array
+// $obj = json_decode($str); // convert object
+
+// var_dump($obj);
+
+//============ Custom object =====================
+// $arr = [1,2,3];
+
+// $obj = (object) $arr;
+
+// var_dump($obj->{1});
+
+var_dump($obj->{1});
