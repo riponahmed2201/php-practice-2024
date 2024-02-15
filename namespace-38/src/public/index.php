02c1c48f91a5d03c59f4e45c581fe0c7367d466b
ripon <riponahmed2201@gmail.com>
2024-02-15 12:46:10 +0600

    namspace added

---
+++ namespace-38/src/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require_once '../PaymentGateway/Paddle/Transaction.php';
+require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+require_once '../PaymentGateway/Stripe/Transaction.php';
+
+use PaymentGateway\Paddle\Transaction;
+
+var_dump(new Transaction());
