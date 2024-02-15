7e42a4e6597b5d329f4ab55b5aa39266798416ff
ripon <riponahmed2201@gmail.com>
2024-02-15 13:32:41 +0600

    namspace done

--- namespace-38/src/PaymentGateway/Paddle/Transaction.php
+++ namespace-38/src/PaymentGateway/Paddle/Transaction.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace PaymentGateway\Paddle;
 
+use Notification\Email;
+
 class Transaction
 {
     public function __construct()
     {
-        var_dump(new CustomerProfile());
+        var_dump(new Email());
     }
 }
--- namespace-38/src/public/index.php
+++ namespace-38/src/public/index.php
@@ -1,10 +1,19 @@
 <?php
 
-
+require_once '../Notification/Email.php';
 require_once '../PaymentGateway/Paddle/Transaction.php';
 require_once '../PaymentGateway/Paddle/CustomerProfile.php';
 require_once '../PaymentGateway/Stripe/Transaction.php';
 
-use PaymentGateway\Paddle\Transaction;
+// use PaymentGateway\Paddle\Transaction;
+// use PaymentGateway\Paddle\CustomerProfile;
 
-var_dump(new Transaction());
+use PaymentGateway\Paddle\{Transaction, CustomerProfile}; // same location or path a hole aivabe {} ar moddhe sob gula lekha jai
+
+use PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+$paddleTransaction = new Transaction();
+$stripeTransaction = new StripeTransaction();
+$paddleCustomerProfile = new CustomerProfile();
+
+var_dump($paddleCustomerProfile, $paddleTransaction, $stripeTransaction);
